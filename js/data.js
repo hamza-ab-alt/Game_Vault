facd183b63ad3005ab99291584e1734fe826accb
hamza aballagh <hamzaaballagh941@gmail.com>
2026-04-01 10:37:32 +0100

    add data the game

--- js/data.js
+++ js/data.js
@@ -1,1 +1,51 @@
-ijvfebuivdsfbjidfv
+// js/data.js
+export const games = [
+    {
+        id: 1,
+        title: "Cyberpunk 2077",
+        price: 59.99,
+        category: "RPG",
+        image: "https://cdn1.epicgames.com/offer/77f2b98e2cef40c8a7437518bf420e47/EGS_Cyberpunk2077PhantomLiberty_CDPROJEKTRED_DLC_S1_2560x1440-c62f1eb1498aaea2fc109b7aa50279a3?resize=1&w=480&h=270&quality=medium",
+        description: "Plongez dans l'avenir sombre de Night City."
+    },
+    {
+        id: 2,
+        title: "Elden Ring",
+        price: 49.99,
+        category: "Action",
+        image: "https://external-game-cover-image-cf.store.on.epicgames.com/119133?resize=1&w=360&h=480&quality=medium",
+        description: "Explorez l'Entre-terre, un vaste monde fantastique."
+    },
+    {
+        id: 3,
+        title: "Modern Warfare III",
+        price: 69.99,
+        category: "FPS",
+        image: "https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/store/games/mw3/overview/Store_GamesPDP_Hero01.png?imwidth=1920",
+        description: "Le combat contre la menace ultime continue."
+    },
+    {
+        id: 4,
+        title: "The Witcher 3",
+        price: 29.99,
+        category: "RPG",
+        image: "https://cdn1.epicgames.com/offer/14ee004dadc142faaaece5a6270fb628/EGS_TheWitcher3WildHuntCompleteEdition_CDPROJEKTRED_S2_1200x1600-53a8fb2c0201cd8aea410f2a049aba3f?resize=1&w=360&h=480&quality=medium",
+        description: "Incarnez Geralt de Riv et parcourez un monde dévasté."
+    },
+    {
+        id: 5,
+        title: "FIFA 25",
+        price: 79.99,
+        category: "Sport",
+        image: "https://external-game-cover-image-cf.store.on.epicgames.com/314499?resize=1&w=360&h=480&quality=medium",
+        description: "Vivez l'expérience de football la plus réaliste."
+    },
+    {
+        id: 6,
+        title: "Starfield",
+        price: 55.00,
+        category: "RPG",
+        image: "https://external-game-cover-image-cf.store.on.epicgames.com/96437?resize=1&w=360&h=480&quality=medium",
+        description: "Explorez les étoiles et découvrez le mystère de l'humanité."
+    }
+];
