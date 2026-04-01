03783828e4fae169a7b2205041ae2797f63e93d8
hamza aballagh <hamzaaballagh941@gmail.com>
2026-04-01 11:40:54 +0100

    worcked in deseign html the home page

---
+++ js/cart.js
@@ -0,0 +1,9 @@
+// js/cart.js
+
+// 1. Récupérer les éléments du DOM
+const cartItemsContainer = document.getElementById('cart-items');
+const cartTotalElement = document.getElementById('cart-total');
+const cartCountBadge = document.getElementById('cart-count');
+const orderSummary = document.getElementById('order-summary');
+const checkoutBtn = document.getElementById('checkout-btn');
+
---
+++ js/main.js
@@ -0,0 +1,6 @@
+import { games } from './data.js';
+
+const gamesGrid = document.getElementById('games-grid');
+const searchInput = document.getElementById('search-input');
+const categoryButtons = document.querySelectorAll('.cat-btn');
+
